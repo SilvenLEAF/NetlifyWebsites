9125d281d25c78c1bf66b8452e86cefab5abf7d5
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-28 12:58:01 +0530

    fully functional Navbar

---
+++ navSite/scripts/main.js
@@ -0,0 +1,26 @@
+const hamBtn = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobileNav');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+
+
+
+hamBtn.addEventListener('click', e=> toggleMobileNav())
+overlay.addEventListener('click', e => toggleMobileNav())
+
+
+
+
+
+
+function toggleMobileNav(){
+  console.log('fired')
+  hamBtn.classList.toggle('open');
+  overlay.classList.toggle('open');
+  mobileNav.classList.toggle('open');
+  
+  body.classList.toggle('noscroll');
+}
+
+
