5230c6c290cbf88b70b99777c27b6ed74da9bbc8
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-29 07:45:58 +0530

    amazing aminated navbar (animating ham+overlay+mobileNav)

---
+++ NavSite2/scripts/main.js
@@ -0,0 +1,20 @@
+const hamBtn = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay');
+const mobileNav = document.querySelector('.mobileNav');
+const body = document.querySelector('body');
+
+
+
+
+hamBtn.addEventListener('click', e=> toggleMobileNav());
+overlay.addEventListener('click', e=> toggleMobileNav());
+
+
+
+
+function toggleMobileNav(){
+  hamBtn.classList.toggle('open');
+  mobileNav.classList.toggle('open');
+  overlay.classList.toggle('open');
+  body.classList.toggle('noscroll');
+}
