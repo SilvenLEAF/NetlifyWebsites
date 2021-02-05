2817e7c7a124f105e0fc24a2868c3e38da6b4465
SilvenLEAF <SilvenLEAF@gmail.com>
2021-02-05 18:06:37 +0530

    created basic skeleton

---
+++ Mrinmoy/scripts/main.js
@@ -0,0 +1,24 @@
+const overlay = document.querySelector('.overlay');
+const mobileNav = document.querySelector('.mobileNav');
+const hamburger = document.querySelector('.hamburger');
+const body = document.querySelector('body');
+const sideNavClose = document.querySelectorAll('.sidenavClose');
+
+
+
+hamburger.addEventListener('click', e=> comeMyNav());
+overlay.addEventListener('click', e=> comeMyNav());
+
+
+for(let i=0; i<sideNavClose.length; i++){  
+  sideNavClose[i].addEventListener('click', e=> comeMyNav());
+}
+
+
+
+function comeMyNav(){
+  overlay.classList.toggle('open');
+  mobileNav.classList.toggle('open');
+  hamburger.classList.toggle('open');
+  body.classList.toggle('noscroll');
+}
